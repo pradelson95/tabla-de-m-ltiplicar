3d8140aa89b5c59f7c7dffe5a2c8371c3cf92c5f
pradelson francois <81245639+pradelson95@users.noreply.github.com>
2021-03-23 10:43:07 -0400

    Add files via upload

---
+++ tabla de multiplicar.py
@@ -0,0 +1,15 @@
+
+def tabladeMultiplicar():
+  global n  
+
+  n = int(input("ingrese que número de la tabla de múltiplicar desea:"))
+
+tabladeMultiplicar()
+
+def tabladeMultiplicar2():
+
+  for i in range(0,11):
+    print(n,'x',i,'=',i*n)
+
+tabladeMultiplicar2()
+  
